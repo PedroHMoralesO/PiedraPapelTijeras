1da48f31041f8382afe5186c2738fb315674db89
PedroDelMaL <pedrohmoraleso@gmail.com>
2020-07-15 19:27:06 -0500

    se cambian las funciones if por switch
    se cambian las funciones if por switch

--- piedra-papel-tijera.js
+++ piedra-papel-tijera.js
@@ -6,12 +6,16 @@ let Checked = null;
 //ciclo for para identificar si mas de un checkbox a sido selecionado en cuyo caso el anterior sera deselecionado en automatico, el ciclo for recorrera los chekboxes y validara si esta en estado true o false guiandose por la clase only-one para identificar cuales elementos va a validar, despues el if validara si esta cekeado y de ser asi el anterior sera des-selecionado
 for (let CheckBox of document.getElementsByClassName('only-one')){
 	CheckBox.onclick = function(){
-  	if(Checked!=null){
-      Checked.checked = false;
-      Checked = CheckBox;
+        switch(Checked){
+            case null:
+                Checked = CheckBox;
+                break;
+            default:
+                Checked.checked = false;
+                Checked = CheckBox;
+                break;
+        }
     }
-    Checked = CheckBox;
-  }
 }
 //////////////////////////////////////////////////////////////////////////////
 // esta linea inicia todo, al dar clic en el boton arrancara la funcion maquina()
@@ -19,17 +23,20 @@ document.getElementById("start").onclick = maquina;
 
 // esta funcion es la inteligencia artifical de la maquina, genera numeros aleatorios del 1 al 10 segun su rango elijira piedra, papel o tijeras: 1 al 4 sera piedra del 5 al 7 sera papel y del 8 al 10 sera tijeras, si alguna de estas condiciones se cumple pasara a la funcion seleccion()
 function maquina(){
-    let numero = Math.floor(Math.random() * (11 - 1)) + 1;
+    let numero = Math.floor(Math.random() * (4 - 1)) + 1;
     console.log(numero)
-    if (numero <= 4){
-        ia ='piedra';
-        seleccion();
-    } else if (numero >= 8) {
-        ia ='tijeras';
-        seleccion();
-    } else {
-        ia = 'papel';
-        seleccion();
+    switch (numero){
+        case 1:
+            ia ='piedra';
+            seleccion();
+            break;
+        case 2:
+            ia ='tijeras';
+            seleccion();
+            break;
+        default:
+            ia = 'papel';
+            seleccion();
     }
 }
 
@@ -40,21 +47,31 @@ function seleccion(){
     let papel = document.getElementById('papel-chk').checked;
     let tijeras = document.getElementById('tijeras-chk').checked;
     
-    if(piedra){
-      usuario = 'piedra';
-      console.log(usuario);
-      ppt(usuario, ia);
-    }else if (papel){
-        usuario = 'papel';
-        console.log(usuario);
-        ppt(usuario, ia);
-    }else if (tijeras) {
-        usuario = 'tijeras';
-        console.log(usuario);
-        ppt(usuario, ia);    
-    } else if (usuario === null){
-        error();
-    }
+    switch(piedra){
+        case true:
+            usuario = 'piedra';
+            console.log(usuario);
+            ppt(usuario, ia);
+            break;
+        default:
+            switch(papel){
+                case true:
+                    usuario = 'papel';
+                    console.log(usuario);
+                    ppt(usuario, ia);
+                    break;
+                default:
+                    switch(tijeras){
+                        case true:
+                            usuario = 'tijeras';
+                            console.log(usuario);
+                            ppt(usuario, ia);
+                            break
+                        default:
+                            error();
+                    }
+            }
+    }                            
 }
 // esta funcion es por si no se selecciono ningun chekbox, lo que hace es escribir en el h2 asignado que por favor se seleccione uno de los checkboxes
 function error(){
@@ -62,29 +79,42 @@ function error(){
 }
 // esta funcion piedra papel tijeras abreviada toma 2 valores dados en la funcion anterior en este caso 'usuario' y 'ia', usuario es validado por un if, en caso de cumplirse entrara a otro siclo de ifs donde se comparara con la variable ia que es el resultado de la funcion maquina, segun los valores de las variables se cumplira 1 de los 9 esenarios diferentes
 function ppt(valor1, valor2){
-    if(usuario === 'piedra'){
-        if(ia === 'papel'){
-            document.getElementById('resultado').innerHTML='La Maquina uso papel, Tu pierdes';
-        }else if(ia === 'tijeras'){
-            document.getElementById('resultado').innerHTML='La Maquina uso tijeras, Tu ganas';
-        }else {
-            document.getElementById('resultado').innerHTML='La Maquina uso piedra, es un empate';
-        }
-    } else if(usuario === 'papel'){
-        if(ia === 'papel'){
-            document.getElementById('resultado').innerHTML='La Maquina uso papel, es un empate';
-        }else if(ia === 'tijeras'){
-            document.getElementById('resultado').innerHTML='La Maquina uso tijeras, Tu pierdes';
-        }else {
-            document.getElementById('resultado').innerHTML='La Maquina uso piedra, Tu ganas';
-        }
-    } else{
-        if(ia === 'papel'){
-            document.getElementById('resultado').innerHTML='La Maquina uso papel, Tu ganas';
-        }else if(ia === 'tijeras'){
-            document.getElementById('resultado').innerHTML='La Maquina uso tijeras, es un empate';
-        }else {
-            document.getElementById('resultado').innerHTML='La Maquina uso piedra, Tu pierdes';
-        }
+    switch(usuario){
+        case 'piedra':
+            switch(ia){
+                case 'papel':
+                    document.getElementById('resultado').innerHTML='La Maquina uso papel, Tu pierdes';
+                    break;
+                case 'tijeras':
+                    document.getElementById('resultado').innerHTML='La Maquina uso tijeras, Tu ganas';
+                    break;
+                default:
+                    document.getElementById('resultado').innerHTML='La Maquina uso piedra, es un empate';
+                    break;
+            }
+        case 'papel':
+            switch(ia){
+                case 'papel':
+                    document.getElementById('resultado').innerHTML='La Maquina uso papel, es un empate';
+                    break;
+                case 'tijeras':
+                    document.getElementById('resultado').innerHTML='La Maquina uso tijeras, Tu pierdes';
+                    break;
+                default:
+                    document.getElementById('resultado').innerHTML='La Maquina uso piedra, Tu ganas';
+                    break;
+            }
+        default:
+            switch(ia){
+                case 'papel':
+                    document.getElementById('resultado').innerHTML='La Maquina uso papel, Tu ganas';
+                    break;
+                case 'tijeras':
+                    document.getElementById('resultado').innerHTML='La Maquina uso tijeras, es un empate';
+                    break;
+                default:
+                    document.getElementById('resultado').innerHTML='La Maquina uso piedra, Tu pierdes';
+                    break;
+            }
     }
 }
